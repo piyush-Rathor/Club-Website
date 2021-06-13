5f64b33706c4e1734912e3f6457a5949a16dd9f1
piyush-Rathor <psrathor16072000@gmail.com>
2021-06-13 23:02:40 +0530

    Updating Mail

--- config/constants.js
+++ config/constants.js
@@ -13,14 +13,32 @@ const JWT_SECRET = {
 };
 
 const selectionMAil = (name) => {
-  return `<div style="text-align:center"><b>Congratulations!</b></div>
-    <div style="text-align:center">${name}</div><br/>
-    <div style="text-align:center">You are Selected for Club<div>`;
+  return `<body style="padding:100px;">
+  <h2>Unnat Technical Club</h2>
+  <h3>Request Accepted!!!</h3>
+  <p>Hey ${name} Your request for membership in the technical team of Unnat technical club has accepted.
+      <br>Now you can login as a member of the club using same credentials and from now you
+      can contribute in the club, can discuss with other club members and authorities.
+
+      <hr>
+     <br> <b>Best of Luck!!!</b>
+  </p>
+</body>`;
 };
 const rejectctionMAil = (name) => {
-  return `<div style="text-align:center">Hey ${name}</div>
-  <div style="text-align:center">You are <b>not</b> Selected for Club</div><br/>
-    <div style="text-align:center">Better luck next Time!</div>`;
+  return `<body style="padding:100px;">
+  <h2>Unnat Technical Club</h2>
+  <h3>Better Luck Next Time!!!</h3>
+  <p>Hey ${name} Your request for membership in the technical team of Unnat technical club has rejected.
+      <br>Unfortunatey you failed to meet cetain criterias and expectations from the club authorities for now.
+      We reuqest you to wait for next club recruitments to apply again for club membership.
+      <br>
+      You can still contribute to the club, by writing your suggestioins on club website.
+
+      <hr>
+      We wish you <b>Best of Luck!!!</b> for next time...
+  </p>
+</body>`;
 };
 
 const removeMemberMAil = (name) => {
@@ -29,4 +47,11 @@ const removeMemberMAil = (name) => {
     <div style="text-align:center">By Unnat technical Club Administrator</div>`;
 };
 
-module.exports = { ...defaultConfig, ...STATUS, ...JWT_SECRET, selectionMAil ,rejectctionMAil,removeMemberMAil};
+module.exports = {
+  ...defaultConfig,
+  ...STATUS,
+  ...JWT_SECRET,
+  selectionMAil,
+  rejectctionMAil,
+  removeMemberMAil,
+};
